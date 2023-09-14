6446655a2e807a468dd7d960ffde812f31f4b3b1
Tanya Sharma <66734041+tanu2519@users.noreply.github.com>
2023-09-14 21:17:59 +0530

    Add files via upload

---
+++ react-multi-carousel/src/data.js
@@ -0,0 +1,62 @@
+export const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 1024 },
+    items: 5,
+    slidesToSlide: 2,
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 800 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 800, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+export const productData = [
+  {   id: 1,
+    name: 'Product 1',
+    imageurl: 'images/p1.jfif',
+    price: 300
+  },
+  { id: 2,
+    name: 'Product 2',
+    imageurl: 'images/p7.jfif',
+  },
+  { id: 3,
+    name: 'Product 3',
+    imageurl: 'images/p3.jfif',
+    price: 1200
+  },
+  { id: 4,
+    name: 'Product 4',
+    imageurl: 'images/p4.jfif',
+    price: 800
+  },
+  { id: 5,
+    name: 'Product 1',
+    imageurl: 'images/p1.jfif',
+    price: 1300
+  },
+  {id: 6,
+    name: 'Product 2',
+    imageurl: 'images/p5.jfif',
+    price: 700
+  },
+  { id: 7,
+    name: 'Product 3',
+    imageurl: 'images/p6.jfif',
+    price: 2300
+  },
+  {id: 8,
+    name: 'Product 4',
+    imageurl: 'images/p7.jfif',
+    price: 900
+  },
+];
